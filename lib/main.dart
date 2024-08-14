0f7ff7ab361d40c87623629fefc846f323079ffb
CESAR_ALEXANDER_GONZALEZ_VALDEZ <a.20308051281060@cbtis128.edu.mx>
2024-08-14 14:16:59 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const TarjetaApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TarjetaApp extends StatelessWidget {
+  const TarjetaApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      debugShowCheckedModeBanner: false,
+      title: 'Tarjeta cesar',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,58 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PaginaInicio(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class PaginaInicio extends StatefulWidget {
+  const PaginaInicio({Key? key}) : super(key: key);
 
+  @override
+  State<PaginaInicio> createState() => _PaginaInicioState();
+}
+
+class _PaginaInicioState extends State<PaginaInicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text("Ejemplo de Tarjetas"),
+        actions: const [Icon(Icons.search), Icon(Icons.more_vert)],
+        backgroundColor: Color(0xffdc0a5a),
+        shape: const StadiumBorder(),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Container(
+          height: 300,
+          width: 300,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide.merge(
+                  BorderSide(
+                      width: 1.5, color: Colors.grey, style: BorderStyle.solid),
+                  BorderSide(
+                      width: 1.5,
+                      color: Colors.grey,
+                      style: BorderStyle.solid)),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            color: const Color(0xffcb118d),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Cesar is Cool',
+                  style:
+                      const TextStyle(color: Color(0xfff8f2fd), fontSize: 30),
+                ),
+                const SizedBox(height: 20),
+                Container(height: 100, width: 100, child: const FlutterLogo()),
+              ],
+            ),
+          ),
         ),
       ),
     );
